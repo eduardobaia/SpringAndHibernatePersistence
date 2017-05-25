ce34497484ad036ddb22088347ebe4c2242aad4e
eduardobaia <eduardo_baia@msn.com>
2017-05-25 16:46:09 -0300

    Ini
    ini

---
+++ src/main/java/com/springAndHibernate/SpringAndHibernate/Persistence/SpringAndHibernatePersistenceApplication.java
@@ -0,0 +1,12 @@
+package com.springAndHibernate.SpringAndHibernate.Persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndHibernatePersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndHibernatePersistenceApplication.class, args);
+	}
+}
